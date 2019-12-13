d7704191f4e1408bc7201cbb034ed6bbf356fe70
wymerkd <wymerkd@gmail.com>
2019-12-13 10:14:20 -0800

    add if else statement to compare first_word and second_word, add instance with empty string to display a message.

--- lib/anagram_logic.rb
+++ lib/anagram_logic.rb
@@ -4,10 +4,17 @@ class Anagram
   def initialize(first_word, second_word)
     @first_word = first_word
     @second_word = second_word
+    @is_it_anagram = ''
   end
 
   def anagram_check
-    @first_word = @first_word.split('').sort
-    @first_word = @first_word.split('').sort
+    @first_word = @first_word.downcase().split('').sort()
+    @second_word = @second_word.downcase().split('').sort()
+    if @first_word == @second_word
+      @is_it_anagram = "These words are anagrams."
+    else
+      @is_it_anagram = "These words are not anagrams."
+    end
   end
+  puts @is_it_anagram
 end

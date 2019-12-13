8c75816e2dca54d01c7b06c07bfcacc93f05b898
wymerkd <wymerkd@gmail.com>
2019-12-13 11:32:10 -0800

    pass test for fourth spec

--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -24,7 +24,7 @@ end
 describe('#word_check') do
   # First test for requirement 3
   it('checks if the inputs are words') do
-    anagram = Anagram.new("ruby", "br")
+    anagram = Anagram.new("ruby", "bury")
     expect(anagram.word_check).to(eq("These are real words."))
   end
 end

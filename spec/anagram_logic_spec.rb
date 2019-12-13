8347ebbe4ac2b5bef37a75fea9272fbbd78efaa4
wymerkd <wymerkd@gmail.com>
2019-12-13 10:28:48 -0800

    fail test for third spec

--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -2,13 +2,21 @@ require('rspec')
 require('anagram_logic')
 
 describe('#anagram_check') do
+  # First test for requirement 1
   it('checks if two words are anagrams') do
-    anagram = Anagram.new("RUBY", "Bury")
+    anagram = Anagram.new("ruby", "bury")
     expect(anagram.anagram_check).to(eq("These words are anagrams."))
   end
 
+  # Second test for requirement 1
   it('checks if two words are not anagrams') do
-    anagram = Anagram.new("RUBY", "rubi")
+    anagram = Anagram.new("ruby", "rails")
     expect(anagram.anagram_check).to(eq("These words are not anagrams."))
+  end
+
+  # First test for requirement 2
+  it('accounts for the possibility that words might have different cases but should still be anagrams') do
+    anagram = Anagram.new("RUBY", "Burry")
+    expect(anagram.anagram_check).to(eq("These words are anagrams."))
   end
 end

e24603e543cdb0fe5612877a6aca0df9a132775a
wymerkd <wymerkd@gmail.com>
2019-12-13 17:59:40 -0800

    remove instance variables from variables that are not used in more than one method

--- lib/anagram_logic.rb
+++ lib/anagram_logic.rb
@@ -4,10 +4,9 @@ class Anagram
   def initialize(first_word, second_word)
     @first_word = first_word.tr(' ~!@#$%^&*()_\+{}|:<>?[]\;,./-','').downcase().split('').sort()
     @second_word = second_word.tr(' ~!@#$%^&*()_\+{}|:<>?[]\;,./-','').downcase().split('').sort()
-    @vowels = ["a", "e", "i", "o", "u", "y"]
   end
 
-#Checks if both first_word and second_word arrays are equal to determine if anagram
+  #Checks if both first_word and second_word arrays are equal to determine if anagram
   def anagram_check
     if @first_word == @second_word
       @is_it_anagram = "These words are anagrams."
@@ -16,24 +15,25 @@ class Anagram
     end
   end
 
-#Combines both first_word and vowels, then combines second_word and vowels. Checks for common occurrences.
+  #Combines both first_word and vowels, then combines second_word and vowels. Checks for common occurrences.
   def word_check
-    first_vowel_check = @vowels & @first_word
-    second_vowel_check = @vowels & @second_word
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    first_vowel_check = vowels & @first_word
+    second_vowel_check = vowels & @second_word
     if first_vowel_check.empty? === false && second_vowel_check.empty? === false
-      @is_it_word = "These are real words."
+      is_it_word = "These are real words."
     else
-    @is_it_word = "You need to input actual words!"
+      is_it_word = "You need to input actual words!"
     end
   end
 
-#Combines both first_word and second_word then looks for common occurrence.
+  #Combines both first_word and second_word then looks for common occurrence.
   def antigram_check
     antigrams = @first_word & @second_word
     if antigrams.empty? === false
-      @is_it_antigram = "These words have letter matches."
+      is_it_antigram = "These words have letter matches."
     else
-      @is_it_antigram = "These words have no letter matches and are antigrams."
+      is_it_antigram = "These words have no letter matches and are antigrams."
     end
   end
 end

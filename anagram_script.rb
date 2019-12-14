2a13b050ebe14b20e23fabf48aad936569f7e79d
wymerkd <wymerkd@gmail.com>
2019-12-13 17:15:05 -0800

    add script for user interaction in terminal.

---
+++ anagram_script.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+require ('./lib/anagram_logic.rb')
+
+puts "Check to see if two words or phrases are anagrams"
+puts "Enter a word or phrase"
+first_word = gets.chomp
+puts "Enter another word or phrase"
+second_word = gets.chomp
+is_it_anagram = Anagram.new(first_word, second_word)
+puts is_it_anagram.anagram_check()
